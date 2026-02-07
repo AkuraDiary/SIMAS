b228230ef928ed43afbc079b371b93b07e95eb34
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2026-02-07 21:04:38 +0700

    using id

--- app/Models/SuratUnit.php
+++ app/Models/SuratUnit.php
@@ -3,9 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class SuratUnit extends Pivot
@@ -13,7 +11,7 @@ class SuratUnit extends Pivot
     /** @use HasFactory<\Database\Factories\SuratUnitFactory> */
     use HasFactory;
     protected $table = 'surat_unit';
-
+    
     protected $fillable = [
         'surat_id',
         'unit_kerja_id',
--- database/migrations/2026_02_07_022236_create_surat_units_table.php
+++ database/migrations/2026_02_07_022236_create_surat_units_table.php
@@ -12,12 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('surat_unit', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('surat_id')->constrained();
             $table->foreignId('unit_kerja_id')->constrained();
             $table->enum('jenis_tujuan', ['utama', 'tembusan']);
             $table->dateTime('tanggal_terima')->nullable();
             $table->enum('status_baca', ['BELUM', 'SUDAH'])->default('BELUM');
-            $table->primary(['surat_id', 'unit_kerja_id']);
+            // $table->primary(['surat_id', 'unit_kerja_id']);
         });
     }
 
