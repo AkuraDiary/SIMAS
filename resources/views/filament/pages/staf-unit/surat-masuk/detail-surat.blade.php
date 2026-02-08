8ae36cce5a4ee04758b9571e93d17d1440292044
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2026-02-08 22:30:53 +0700

    revamp disposisi

--- resources/views/filament/pages/staf-unit/surat-masuk/detail-surat.blade.php
+++ resources/views/filament/pages/staf-unit/surat-masuk/detail-surat.blade.php
@@ -3,47 +3,61 @@
     @php
     $userUnitId = auth()->user()->unit_kerja_id;
     @endphp
-
     <x-filament::section collapsible>
-        <x-slot name="heading">Lembar Disposisi</x-slot>
+        <x-slot name="heading">Alur Disposisi</x-slot>
+        <x-slot name="description">
+            Riwayat disposisi surat
+        </x-slot>
 
-        @forelse ($disposisiUntukSaya as $disposisi)
-        <div class="border rounded p-4 space-y-2">
-            <hr>
-            <br>
-            <p><strong>Tanggal Disposisi:</strong> {{ $disposisi->tanggal_disposisi }}</p>
-            <p><strong>Asal:</strong> {{ $disposisi->pembuat->unitKerja->nama_unit }}</p>
-            <p><strong>Instruksi:</strong> {{ $disposisi->jenis_instruksi }}</p>
-            <p><strong>Sifat:</strong> {{ $disposisi->sifat }}</p>
-            <p><strong>Catatan:</strong><br>{{ $disposisi->catatan }}</p>
-            <p class="text-sm text-gray-500">
-                <strong>Status:</strong> {{ $disposisi->status_disposisi }}
-            </p>
-        </div>
-        @empty
-        <p class="text-gray-500 italic">
-            Tidak ada disposisi untuk unit ini.
+        @php
+        $userUnitId = auth()->user()->unit_kerja_id;
+        @endphp
+
+        @if ($surat->disposisis->isEmpty())
+        <p class="text-sm text-gray-500 italic">
+            Surat ini belum memiliki disposisi.
         </p>
-        @endforelse
-
-        <br>
-        <hr>
-        <br>
-        @if ($disposisiLainnya->isEmpty())
-        <p class="text-gray-500 italic">Tidak ada disposisi untuk unit lain.</p>
         @else
-        <ul class="list-disc pl-5">
-            @foreach ($disposisiLainnya as $d)
-            <li>
+        @foreach ($surat->disposisis->sortBy('tanggal_disposisi') as $d)
+        @php
+        $isForMe = $d->unit_tujuan_id === $userUnitId;
+        $prefix = $d->parent_disposisi_id ? '↳ ' : '';
+        @endphp
+
+        <p class="{{ $isForMe ? 'font-semibold text-primary-600' : '' }}">
+            {{ $prefix }}
+            <strong>
+                {{ $d->pembuat->unitKerja->nama_unit }}
+                →
                 {{ $d->unitTujuan->nama_unit }}
-                ({{ $d->status_disposisi }})
-            </li>
-            @endforeach
-        </ul>
+            </strong>
+            <span class="text-sm text-gray-500">
+                ({{ $d->tanggal_disposisi }})
+            </span>
+        </p>
+
+        <p class="ml-4">
+            <strong>Instruksi:</strong> {{ $d->jenis_instruksi }}
+        </p>
+
+        <p class="ml-4">
+            <strong>Sifat:</strong> {{ ucfirst($d->sifat) }}
+            |
+            <strong>Status:</strong> {{ ucfirst($d->status_disposisi) }}
+        </p>
+
+        @if ($d->catatan)
+        <br>
+        <p class="ml-4 text-gray-600 italic">
+            {{ $d->catatan }}
+        </p>
         @endif
 
-
+        <br>
+        @endforeach
+        @endif
     </x-filament::section>
+
 
     <x-filament::section>
         <x-slot name="heading">
@@ -64,13 +78,11 @@
         </p>
 
         <br>
-        <hr>
-        <br>
 
-        {!! nl2br(e($surat->isi_surat)) !!}
+        <div class="prose max-w-none">
+            {!! nl2br(e($surat->isi_surat)) !!}
+        </div>
 
-        <br><br>
-        <hr>
         <br>
 
         <p><strong>Lampiran</strong>
